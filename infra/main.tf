d455a2577b3bbcb71a601f212529b5681a21993d
ahmed <ahmedtallat641@gmail.com>
2026-06-08 10:06:37 +0000

    push

--- infra/main.tf
+++ infra/main.tf
@@ -6,3 +6,46 @@ resource "aws_vpc" "main" {
     Name = var.vpc_name
   }
 }
+
+# This tells AWS to build a brand new subnet
+
+resource "aws_subnet" "digi" {
+  vpc_id            = aws_vpc.main.id 
+  cidr_block        = var.subnet_cider           
+  availability_zone = "eu-west-1"
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
+ 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "routetable" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "route table"
+  }
+}
+
+resource "aws_route_table_association" "routetableas" {
+  subnet_id      = aws_subnet.digi.id
+  route_table_id = aws_route_table.routetable.id
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.routetable.id
+  destination_cidr_block = var.cidr_block                # Target traffic (all internet)
+  gateway_id             = aws_internet_gateway.gw.id  # Target destination component
+}
